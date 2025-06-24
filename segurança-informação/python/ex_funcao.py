c611431ba76bd95140df52f6c5e7df5158124970
EnzoArrue <enzoarruejuanfuso@gmail.com>
2025-06-24 15:58:17 -0300

    Update and rename ex_função1 to ex_funcao.py

---
+++ segurança-informação/python/ex_funcao.py
@@ -0,0 +1,10 @@
+def media(n1,n2,n3):
+    return (n1+n2+n3)/3
+
+numero1 = float(input("Digite o primeiro valor: "))
+numero2 = float(input("Digite o segundo valor: "))
+numero3 = float(input("Digite o terceiro valor: "))
+
+m = media(numero1,numero2,numero3)
+
+print(f"A média é: {m}")
