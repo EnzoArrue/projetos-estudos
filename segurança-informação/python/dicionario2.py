6345b7f8823a6043eafaa33f0c030cd077f083aa
EnzoArrue <enzoarruejuanfuso@gmail.com>
2025-06-24 16:58:14 -0300

    Update dicionario2.py

--- segurança-informação/python/dicionario2.py
+++ segurança-informação/python/dicionario2.py
@@ -1,1 +1,17 @@
+#Preencha um dicionario com as informações de 5 produtos, utilize o nome como chave e o preço como valor, solicite dados ao usuario, percorra o dicionario e exiba o nome dos produtos com preço maior que 50,00.
+produtos = dict()
+for i in range (5):
+    nome, preco = input("Nome e preço: ").split()
+    preco = float(preco)
+    produtos[nome] = preco
+    print()
+
+print('-' * 30)
+print("Produtos com preço acima de R$50:")
+print("-" * 30)
+for item in produtos.items():
+    nome, preco = item
+    if preco > 50.00:
+        print(f"Nome: {nome}")
+    
 
