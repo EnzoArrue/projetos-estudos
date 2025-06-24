c9e629d106435e99c72144fa33997a7a323aae15
EnzoArrue <enzoarruejuanfuso@gmail.com>
2025-06-24 17:03:56 -0300

    Update dicionario4.py

--- segurança-informação/python/dicionario4.py
+++ segurança-informação/python/dicionario4.py
@@ -1,1 +1,12 @@
+#Crie uma função que conte a quantidade de cada vogal em um texto digitado pelo usuário.
+#Dicionário
+def vogais(texto):
+    d = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
+    for letra in texto:
+        if letra in "aeiou":
+            d[letra] += 1
+    return d
+
+texto = input()
+print(vogais(texto))
 
