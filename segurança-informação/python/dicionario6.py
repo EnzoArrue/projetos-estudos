8fe7e7ab2447623ef2c1ee5b601932dfacf7d921
EnzoArrue <enzoarruejuanfuso@gmail.com>
2025-06-24 17:07:20 -0300

    Update dicionario6.py

--- segurança-informação/python/dicionario6.py
+++ segurança-informação/python/dicionario6.py
@@ -1,1 +1,15 @@
+#Cadastre o RA e três notas de 5 alunos em um dicionário e exiba a média de cada um.
+alunos = {}
+for i in range(5):
+    RA = int(input("RA:"))
+    n1 = float(input("N1: "))
+    n2 = float(input("N2: "))
+    n3 = float(input("N3: "))
+    notas =[n1, n2, n3]
+    alunos[RA]= [n1, n2, n3] #ou alunos[RA]= notas
+
+for aluno in alunos.items():
+    RA, notas = aluno
+    media = (notas[0] + notas[1] + notas[2])/ 3
+    print(f"Média: {media:.2f}")
 
