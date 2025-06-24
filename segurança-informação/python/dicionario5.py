6427c75a8b82d109209dbf96837b270fc3bfdad9
EnzoArrue <enzoarruejuanfuso@gmail.com>
2025-06-24 17:04:58 -0300

    Update dicionario5.py

--- segurança-informação/python/dicionario5.py
+++ segurança-informação/python/dicionario5.py
@@ -1,1 +1,21 @@
+#Preencha um dicionário com as informações de 5 produtos
+#-	Utilize o nome do produto como chave e o preço como valor.
+#-	Solicite os dados ao usuário
+#-	Percorra o dicionário e exiba o nome dos produtos com preço 
+#superior a R$ 50,00.
+
+#Exemplo:
+#Veja um exemplo da estrutura do dicionário.
+#{'mouse': 80.0, 'caneta': 3.0, 'Pen drive': 100.0,'Teclado': 30.0, 'Lápis': 1.5}
+
+produtos = {}
+for i in range(5):
+    nome=input("Nome: ")
+    preco=float(input("Valor: "))
+    produtos[nome]=preco
+
+for nome, preco in produtos.items():
+    if preco > 50.00:
+        print(f"{nome}:{preco:.2f}", end=' ')
+                
 
